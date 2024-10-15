b15bd35c3795d3ac4b2c3bb6c6642194c0f1356f
Dinika03 <78576083+Dinika03@users.noreply.github.com>
2024-10-14 23:27:34 -0400

    Update CEDDL.js

--- CEDDL.js
+++ CEDDL.js
@@ -6,6 +6,8 @@ EDC.datalayerObj = 'digitalData';
 if (!window.digitalData) {
     window[EDC.datalayerObj] = {};
     window[EDC.datalayerObj].page = window[EDC.datalayerObj].page || {};
+    window[EDC.datalayerObj].user.segment = window[EDC.datalayerObj].page.category || {};
+    window[EDC.datalayerObj].user.segment = window[EDC.datalayerObj].page.pageInfo || {};
     window[EDC.datalayerObj].events = window[EDC.datalayerObj].events || [];
     window[EDC.datalayerObj].user = window[EDC.datalayerObj].user || {};
     window[EDC.datalayerObj].user.segment = window[EDC.datalayerObj].user.segment || {};
@@ -17,6 +19,42 @@ EDC.utils = EDC.utils || new function () {
 	if (window[window.EDC.datalayerObj] && objCEDDL) {
             window[window.EDC.datalayerObj].events = [];
             window[window.EDC.datalayerObj].events.push(objCEDDL);
+        }
+    };
+
+      this.pageInfoTracking = function (objCEDDL, join) {
+        if (window[window.EDC.datalayerObj] && objCEDDL) {
+            if (!window[window.EDC.datalayerObj].page) {
+                window[window.EDC.datalayerObj].page = {};
+            }
+
+            if (!window[window.EDC.datalayerObj].page.pageInfo) {
+                window[window.EDC.datalayerObj].page.pageInfo = {};
+            }
+
+            if (!join) {
+                window[window.EDC.datalayerObj].page.pageInfo = objCEDDL;
+            } else {
+                (window[window.EDC.datalayerObj].page.pageInfo).extend((window[window.EDC.datalayerObj].page.pageInfo), objCEDDL);
+            }
+        }
+    };
+
+    this.pageCategoryTracking = function (objCEDDL, join) {
+        if (window[window.EDC.datalayerObj] && objCEDDL) {
+            if (!window[window.EDC.datalayerObj].page) {
+                window[window.EDC.datalayerObj].page = {};
+            }
+
+            if (!window[window.EDC.datalayerObj].page.category) {
+                window[window.EDC.datalayerObj].page.category = {};
+            }
+
+            if (!join) {
+                window[window.EDC.datalayerObj].page.category = objCEDDL;
+            } else {
+                (window[window.EDC.datalayerObj].page.category).extend((window[window.EDC.datalayerObj].page.category), objCEDDL);
+            }
         }
     };
 }
